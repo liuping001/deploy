c67401805d0ffbe1a4feba25fe9988214040bc3c
tbag <692603114@qq.com>
2020-02-08 12:47:10 +0800

    install cron

--- d.sh
+++ d.sh
@@ -4,6 +4,7 @@ usage()
     echo "Usage: $0 [server_define.yml] [host.txt] [start] [server_name]... "
     echo "example:"
     echo "    $0 server_define.yml host.txt push server1 server2"
+
 }
 CUR_DIR=`pwd`
 if [ $# -lt 4 ];then
@@ -15,6 +16,6 @@ for server in ${@:4};
 do
     # echo $server
     export ANSIBLE_FORCE_COLOR=true
-    ansible-playbook -i ${CUR_DIR}/$2 ~/.bin/task.yml --tags="$3" -e "server_name=$server server_define_file=${CUR_DIR}/$1 work_dir=${CUR_DIR}" |grep -v -P 'PLAY|unreachable|WARN'
+    ansible-playbook -i ${CUR_DIR}/$2 ~/.bin/task.yml --tags="$3" -e "server_name=$server server_define_file=${CUR_DIR}/$1 work_dir=${CUR_DIR}" |grep -v -P 'PLAY|WARN'
 done;
  
