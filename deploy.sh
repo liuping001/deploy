2fed912aca91eb8df79dc0df6382fdc236b512f9
liuping001 <692603114@qq.com>
2021-05-24 14:52:04 +0800

    deploy_vars: 增加key value变量文件

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,12 @@ usage()
     echo ""
     echo "Usage: deploy server1,server2 action1,action2"
     echo "example:"
-    echo "    deploy server,server2 push"
-    echo "    deploy server,server2 push"
-    echo "    deploy hello push,cmd"
+    echo "    deploy server1,server2 push"
+    echo "    deploy server1 cmd,start"
 }
 
-if [ $# -lt 2 ];then
-    usage
-    exit
-fi
 
 # 获取当前工程根目录
-current_dir=`pwd`
 proj_dir=`pwd`
 find_work_dir_count=10
 while [ ! -d $proj_dir/deploy ]
@@ -33,9 +27,23 @@ deploy_dir=${proj_dir}/deploy
 proj_dir=`cd $proj_dir && pwd`
 deploy_dir=`cd $deploy_dir && pwd`
 
+current_dir=`pwd`
+deploy_vars_file=~/.deploy_vars.yml
+if [ -f ${current_dir}/deploy_vars.yml ];then
+  deploy_vars_file=${current_dir}/deploy_vars.yml
+fi
+
+echo -e "\033[32m"
 echo "info: "
-echo "-> deploy dir is [$deploy_dir]"
-echo "-> project dir is [$proj_dir]"
+echo "-> project path [$proj_dir]"
+echo "-> deploy path [$deploy_dir]"
+echo "-> deploy vars path [$deploy_vars_file]"
+
+if [ $# -lt 2 ];then
+  echo -e "\033[0m"
+  usage
+  exit
+fi
 
 server_list=$(echo $1 | tr "," "\n")
 action_list=$(echo $2 | tr "," "\n")
@@ -43,10 +51,12 @@ action_list=$(echo $2 | tr "," "\n")
 echo "-> action: "$action_list
 echo "-> server: "$server_list
 
+echo -e "\033[0m"
+
 for server in ${server_list}
 do
   for action in ${action_list}
     do
-      ansible-playbook -i ${current_dir}/inventory ~/.bin/task.yml --tags="$2" -e "server_name=$server work_dir=${proj_dir}"
+      ansible-playbook -i ${current_dir}/inventory ~/.bin/task.yml --tags="$2" -e "server_name=$server work_dir=${proj_dir} deploy_vars_file=${deploy_vars_file}"
     done
 done
